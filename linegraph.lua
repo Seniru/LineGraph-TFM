bde20648f6f123661a4df0c1961ada7e21c64277
Seniru Pasan Indira <senirupasan@gmail.com>
2019-08-24 14:57:08 +0530

    Added labels and bug fixes
    Added labels for origin, min and max and median values of x and y
    Bug fixes
     - When 2 or more Graphs are displayed the lines are not shown

--- linegraph.lua
+++ linegraph.lua
@@ -34,6 +34,7 @@ end
 
 local LineChart = {}
 LineChart.__index = LineChart
+LineChart.__joints = 10000
 
 setmetatable(LineChart, {
   __call = function (cls, ...)
@@ -56,16 +57,31 @@ function LineChart.new(id, x, y, w, h, dataX, dataY)
   self.maxY = getMax(dataY)
   self.xRange = self.maxX - self.minX
   self.yRange = self.maxY - self.minY
+  self.joints = LineChart.__joints
+  LineChart.__joints = LineChart.__joints + 10000
+  print(LineChart.joints)
   return self
 end
 
 function LineChart:show(target)
+  --the graph plot
   ui.addTextArea(10000 + self.id, "", target, self.x, self.y, self.w, self.h, nil, nil, 0.5, true)
-  local joints = 1
+  --label of the origin
+  ui.addTextArea(11000 + self.id, "<b>[" .. math.floor(self.minX) .. ", "  .. math.floor(self.minY) .. "]</b>", target, self.x - 15, self.y + self.h + 5, 50, 50, nil, nil, 0, true)
+  --label of the x max
+  ui.addTextArea(12000 + self.id, "<b>" .. math.ceil(self.maxX) .. "</b>", target, self.x + self.w + 10, self.y + self.h + 5, 50, 50, nil, nil, 0, true)
+  --label of the y max
+  ui.addTextArea(13000 + self.id, "<b>" .. math.ceil(self.maxY) .. "</b>", target, self.x - 15, self.y - 10, 50, 50, nil, nil, 0, true)
+  --label x median
+  ui.addTextArea(14000 + self.id, "<b>" .. math.ceil((self.maxX + self.minX) / 2) .. "</b>", target, self.x + (self.w - self.x) / 2, self.y + self.h + 5, 50, 50, nil, nil, 0, true)
+  --label y median
+  ui.addTextArea(15000 + self.id, "<br><br><b>" .. math.ceil((self.maxY + self.minY) / 2) .. "</b>", target, self.x - 15, self.y + (self.h - self.y) / 2, 50, 50, nil, nil, 0, true)
+
+  local joints = self.__joints
   local xRatio = self.w / self.xRange
   local yRatio = self.h / self.yRange
   for d = 1, #self.dataX, 1 do
-    tfm.exec.addJoint(10000 + joints ,-1,-1,{
+    tfm.exec.addJoint(10000 + self.id + joints ,-1,-1,{
       type=0,
       point1= math.floor(self.dataX[d] * xRatio  + self.x - (self.minX * xRatio)) .. ",".. math.floor(invertY(self.dataY[d] * yRatio) + self.y - self.h + (self.minY * yRatio)),
       point2=  math.floor((self.dataX[d+1]  or self.dataX[d]) * xRatio + self.x - (self.minX * xRatio)) .. "," .. math.floor(invertY((self.dataY[d+1] or self.dataY[d]) * yRatio) + self.y - self.h + (self.minY * yRatio)),
@@ -78,13 +94,3 @@ function LineChart:show(target)
     joints = joints + 1
   end
 end
-
-function f(n)
-  return math.sin(n)
-end
-
-local x = {-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5}
-local y = map(x, f)
-
-local chart = LineChart(1, 50, 50, 600, 200, x, y)
-chart:show()
